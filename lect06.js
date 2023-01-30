94c9df56c4744ec801c3497bbd79ae6d16386ff8
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2023-01-30 21:10:33 +0530

    Create lect06.js

---
+++ lect06.js
@@ -0,0 +1,61 @@
+//chapter 2
+//lect 06;
+
+7;
+"hello ankit";
+
+console.log("Operators in js");
+//arithmetic operators
+//+, -, *, **, /, %, ++, --;
+
+let a=5;
+let b=2;
+console.log("a + b = " ,a + b);
+console.log("a - b = " ,a - b);
+console.log("a * b = " ,a * b);
+console.log("a / b = " ,a / b);
+console.log("a ** b = " ,a ** b);
+console.log("a % b = " ,a % b);
+console.log("a++ = " ,a++);
+console.log("a = " ,a);
+console.log("a-- = " ,a--);
+console.log("a = " ,a);
+
+
+//assignment operators
+//=, +=, -=, *= , /=, %=, **=
+//=
+let x=5;
+let y=2;
+//+=
+x+=y;
+console.log(x);
+
+//comparison operators
+//==, !=, ===, !==, >, <, >=, <=, ? ternary operator
+
+let comp1=6;
+let comp2=7;
+console.log("comp1 == comp2",comp1 == comp2);
+console.log("comp1 != comp2",comp1 != comp2);
+console.log("comp1 === comp2",comp1 === comp2);
+console.log("comp1 !== comp2",comp1 !== comp2);
+
+let comp11=6;
+let comp22='6'
+console.log("comp11 == comp22",comp11 == comp22);
+console.log("comp11 != comp22",comp11 != comp22);
+console.log("comp11 === comp22",comp11 === comp22);
+console.log("comp11 !== comp22",comp11 !== comp22);
+
+let comp111=6;
+let comp222=7;
+console.log("comp111 > comp222 = ",comp111>comp222);
+
+//logical operators
+//&&, ||, !
+let p=5;
+let q=6;
+console.log(p<q && p==5);
+console.log(p>q || p==5);
+console.log(!false);
