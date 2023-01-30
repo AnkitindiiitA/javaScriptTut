4cb8f88703699ce0ac640550b293f8959030fd61
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2023-01-30 18:32:51 +0530

    Create lect02.js

---
+++ lect02.js
@@ -0,0 +1,20 @@
+//lect 02
+
+let a=5;
+console.log(a);
+a="ankit";
+console.log(a);
+/*output:5
+         ankit*/
+
+var b=5;
+console.log(b);
+b="ankit";
+console.log(b);
+/*output:5
+         ankit*/
+
+//let 8ankit=7 //this will throw an error
+//js reserved words cann't be used as a variable name
+//java script is case sensitive Ankit and ankit both are different
+
