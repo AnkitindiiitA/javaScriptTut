18df22bb096fca6fa26e3bcae1eca68e7b0a2c5f
AnkitindiiitA <97433903+AnkitindiiitA@users.noreply.github.com>
2023-01-31 19:40:15 +0530

    Create lect10.js

---
+++ lect10.js
@@ -0,0 +1,19 @@
+//lect 10
+//while loop
+
+let n=prompt("enter number ");
+
+n=Number.parseInt(n);
+
+let i=0;
+while(i<n){
+  console.log(i+1);
+  i++;
+}
+
+//do while loop
+i=10;
+do{
+  console.log(i+1);
+  i++;
+}while(i<n)
